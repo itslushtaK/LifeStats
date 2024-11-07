014002c1130d91942fdd4e649ecb00907bf9102e
Gentuar <g.lushtaku@code-invention.com>
2024-11-07 12:27:21 +0200

    htfx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"; // Ensure you have your custom styles here
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to LifeStats</h1>
+        <p>Your life stats and world data.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
